3dde9c937a4dbac21ccf4a236f6a3a83ad1ca046
Natalies-Angels <natalie.shikuku@student.moringaschool.com>
2023-09-09 13:41:44 +0300

    Completed Assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-function Question({ question, onAnswered }) {
+function Question({ question, onAnswered })
+ {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() =>
+  {
+    const timer = setTimeout(() =>
+    { 
+      if(timeRemaining > 0)
+        {
+          setTimeRemaining(timeRemaining -1)
+        }
+      else if(timeRemaining === 0)
+        {
+          setTimeRemaining(10);
+          onAnswered(false);        
+        }
+    },1000)
 
-  function handleAnswer(isCorrect) {
+    return function cleanUp()
+    {
+      clearTimeout(timer);
+    }
+  }, [timeRemaining])
+
+  function handleAnswer(isCorrect)
+   {
     setTimeRemaining(10);
     onAnswered(isCorrect);
-  }
+    }
 
   const { id, prompt, answers, correctIndex } = question;
 
@@ -16,7 +37,8 @@ function Question({ question, onAnswered }) {
     <>
       <h1>Question {id}</h1>
       <h3>{prompt}</h3>
-      {answers.map((answer, index) => {
+      {answers.map((answer, index) => 
+      {
         const isCorrect = index === correctIndex;
         return (
           <button key={answer} onClick={() => handleAnswer(isCorrect)}>
